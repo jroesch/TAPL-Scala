5e8a50be1d40b0ca9d26ac333f4937c603a24f0b
Jared Roesch <roeschinc@gmail.com>
2013-07-30 23:45:10 -0700

    basic Arith.scala def

--- Arith.scala
+++ Arith.scala
@@ -15,3 +15,49 @@ object Arith {
     case Succ(_, t) => isNumericValue(t)
     case _          => false
   }
+
+  def isValue(term: Term) = term match {
+    case True(_) => true
+    case False(_) => true
+    case _ if isNumericValue(term) => true
+    case _ => false
+  }
+
+  case object NoRuleApplies extends Exception("No rule applies.")
+
+  val dummyInfo = "dummy"
+
+  def step(t: Term): Term = match t {
+    case If(_, True(_), trueB, _)  => trueB
+    case If(_, False(_), _, falseB => falseB
+    case If(fi, guard, trueB, falseB) =>
+      val guardN = step(guard)
+      If(info, guardN, trueB, falseB)
+    case Succ(info, pred)
+      val predN = step(pred)
+      Succ(info, predN)
+    case Pred(_, Zero(_)) =>
+      Zero(dummyInfo)
+    case Pred(_, Succ(_, pred)) if isNumericValue(pred) =>
+      pred
+    case Pred(info, succ) =>
+      val succN = step(succ)
+      Pred(info, succ)
+    case Zero(_, Zero(_)) =>
+      True(dummyInfo)
+    case IsZero(_, Succ(_, pred) if isNumericValue(pred) =>
+        TmFalse(dummyInfo)
+    case IsZero(info, nat) =>
+      val natN = step(nat)
+      IsZero(info, nat)
+    case _ => throw NoRuleApplies
+  }
+
+  def eval(term: Term): Term = try {
+    val termS = step(term)
+    eval(termS)
+  } catch {
+    case NoRuleApplies => term
+  }
+}
+
